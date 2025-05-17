127d8ef70e0d6235a2921402c3764c618f93f04c
Boedi15 <budi.priyatin@gmail.com>
2025-05-17 17:29:13 +0700

    Update push to GIT

--- script/git-push.sh
+++ script/git-push.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+echo
+echo "############################################"
+echo "Cek status file yang berubah / belum dilacak"
+echo "############################################"
 git status
+echo
+echo "##############################"
+echo "Tambahkan file ke staging area"
+echo "##############################"
 git add .
+echo
+echo "################"
+echo "Commit perubahan"
+echo "################"
 git commit -m "Update push to GIT"
+echo
+echo "#####################"
+echo "Push ke branch remote"
+echo "#####################"
 git push -u origin main
